bf91943ae700afa4cb1f654a1b44092dde831d70
Arturo Amador Paulino <amadorassassin@gmail.com>
2020-03-24 22:19:03 -0600

    practica de busqueda

---
+++ AngularBuscadorPg/angularbuscador/src/app/componetes/buscador/resconsolas/resconsolas.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { Consola, ConsolasService } from "../../../servicios/consolas.service";
+
+@Component({
+  selector: 'app-resconsolas',
+  templateUrl: './resconsolas.component.html',
+  styleUrls: ['./resconsolas.component.css']
+})
+export class ResconsolasComponent implements OnInit {
+
+  consolas: Consola[] = [];
+  palabrasBusqueda: string;
+
+  constructor(private activatedRoute:ActivatedRoute, private consolasService: ConsolasService) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      console.log("resconsola: ",params['palabrasBusqueda']);
+      this.palabrasBusqueda = params['palabrasBusqueda'];
+      this.consolas = this.consolasService.buscarConsolas(this.palabrasBusqueda);
+      console.log(this.consolas);
+    });
+  }
+
+}
