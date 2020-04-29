f19d0cda4da3bb1b2707f299df7a8ee232a947c2
Arturo Amador Paulino <amadorassassin@gmail.com>
2020-04-28 20:28:25 -0500

    update

---
+++ MEANStack/meanstack/src/app/componentes/alumnos/alumnos.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { AlumnosService } from '../../servicios/alumnos.service';
+
+@Component({
+  selector: 'app-alumnos',
+  templateUrl: './alumnos.component.html',
+  styleUrls: ['./alumnos.component.css']
+})
+export class AlumnosComponent implements OnInit {
+
+  alumnosAsincrono: any;
+
+  constructor(public  alumnosService: AlumnosService) {
+
+    this.alumnosAsincrono = new Promise((resolve, reject) => {
+      this.alumnosAsincrono.getAlumnos().subscribe( alumnos => {
+        console.log(alumnos);
+        resolve(alumnos);
+      });
+    });
+
+  }
+
+  buscarAlumnos(palabra: string) {
+    if (palabra.length > 0){
+      this.alumnosAsincrono  = new Promise((resolve, reject) => {
+        this.alumnosAsincrono.getAlumnosPalabraBusqueda().subscribe( alumnos => {
+          console.log(alumnos);
+          resolve(alumnos);
+        });
+      });
+    }
+  }
+
+  ngOnInit(): void {
+  }
+
+}
