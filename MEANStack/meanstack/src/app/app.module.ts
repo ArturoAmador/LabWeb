4a6f179f7d2dfcfa65521975b64101ebb4344706
Arturo Amador Paulino <amadorassassin@gmail.com>
2020-04-28 20:28:41 -0500

    update

--- MEANStack/meanstack/src/app/app.module.ts
+++ MEANStack/meanstack/src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AlumnosComponent } from './componentes/alumnos/alumnos.component';
+
+import { AlumnosService } from './servicios/alumnos.service';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlumnosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    AlumnosService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- MEANStack/meanstack/src/app/servicios/alumnos.service.ts
+++ MEANStack/meanstack/src/app/servicios/alumnos.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
 export class AlumnosService {
 
-  constructor() { }
+  constructor(public httpClient: HttpClient) {
+    console.log('Servicio creado...');
+  }
+
+  getAlumnos() {
+    const servicioRest = 'http://localhost:8585/students';
+    return this.httpClient.get(servicioRest);
+  }
+
+  getAlumnosPalabraClave(palabraClave:string) {
+    // Utilizamos template literals
+    const servicioRest = `http://localhost:8585/students/busqueda/${palabraClave}`;
+    return this.httpClient.get(servicioRest);
+  }
+
 }
